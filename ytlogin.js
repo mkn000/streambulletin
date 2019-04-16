4ff76ac8e2a3f0d9a47042a01f2970bf97215e58
mkn000 <39862711+mkn000@users.noreply.github.com>
2019-04-16 19:49:21 +0300

    Add files via upload

---
+++ ytlogin.js
@@ -0,0 +1,18 @@
+let token;
+let ydb;
+window.indexedDB.open("swpushnotificationsdb",1).onsuccess = function(event){
+	    ydb= event.target.result;
+	    ydb.transaction(["swpushnotificationsstore"])
+		.objectStore("swpushnotificationsstore")
+		.get("IDToken").onsuccess = function(event){
+		    token = event.target.result.value;
+		    let ythead = {'X-Youtube-Identity-Token':token,
+				'X-Youtube-Client-Name':1,
+				'X-Youtube-Client-Version':2.20190410
+				 }
+		    browser.storage.local.set({ytauth:ythead});
+		}
+}    
+
+//client name: 1
+//client version: 2.20190410
